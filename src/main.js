3f31f6db177ff1ec205a111a4bd0c69aa294b228
Aman Pandey <amanpandey9299@gmail.com>
2026-06-28 22:46:03 +0530

    update src/main.js

--- src/main.js
+++ src/main.js
@@ -3,11 +3,33 @@ import Nexus from "./core/nexus.js";
 const PLUGIN_ID = "com.mrredhood.acode.nexus";
 let nexus = null;
 
+console.log("TOP LEVEL ACODE:", acode);
+console.log("TOP LEVEL ACODE KEYS:", Object.keys(acode));
+console.log(
+  "HAS setPluginInit:",
+  typeof acode.setPluginInit
+);
+console.log(
+  "HAS fileBrowser:",
+  typeof acode.fileBrowser
+);
+console.log(
+  "HAS require:",
+  typeof acode.require
+);
+
 function init(baseUrl, page, options) {
   console.log("NEXUS INIT START");
   console.log("baseUrl:", baseUrl);
   console.log("page:", page);
   console.log("options:", options);
+
+  console.log("INIT ACODE:", acode);
+  console.log("INIT ACODE KEYS:", Object.keys(acode));
+  console.log(
+    "INIT fileBrowser:",
+    typeof acode.fileBrowser
+  );
 
   try {
     nexus = new Nexus(baseUrl, page, options);
