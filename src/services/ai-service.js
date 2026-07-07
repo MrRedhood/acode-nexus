ab86a720cb121b2ae9b13b95a9e52cb9f34e4784
Aman Pandey <amanpandey9299@gmail.com>
2026-07-07 06:25:28 +0530

    update src/services/ai-service.js

--- src/services/ai-service.js
+++ src/services/ai-service.js
@@ -7,6 +7,7 @@ import PromptService from "./prompt-service.js";
 import MessagePreprocessorService from "./message-preprocessor-service.js";
 import ProviderService from "./provider-service.js";
 
+// TEST
 export default class AIService {
   static async getModels(
     provider = null,
